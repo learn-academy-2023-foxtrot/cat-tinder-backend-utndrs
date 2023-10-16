3c44d56d429779aaebeb591b94d9667fb51552de
RonielOverton <ronielo86@gmail.com>
2023-10-16 16:35:29 -0700

    added api endpoints but with a blocker

--- app/controllers/utndrs_controller.rb
+++ app/controllers/utndrs_controller.rb
@@ -1,2 +1,12 @@
 class UtndrsController < ApplicationController
+    def index
+        utndrs = Utndr.all 
+        render json: utndrs
+    end
+    def create
+    end
+    def update
+    end
+    def destroy
+    end
 end
--- spec/requests/utndrs_spec.rb
+++ spec/requests/utndrs_spec.rb
@@ -2,6 +2,18 @@ require 'rails_helper'
 
 RSpec.describe "Utndrs", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it "gets a list of utndr's" do
+      Utndr.create(
+        name: 'Paularts',
+        age: 26,
+        content: 'Arts & Crafts',
+        image: 'https://unsplash.com/photos/ia1p6fqftnQ')
+        get'/utndrs'
+        utndr = JSON.parse(response.body)
+        expect(response).to have_http_status(200)
+        expect(utndr.length).to eq 1
+      
+      end
+
   end
 end
